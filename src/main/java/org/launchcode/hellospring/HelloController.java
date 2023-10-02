ef9836bef847359edeb7742f6319cd1396f0ac52
erinmuzughi <erinmuzughi@gmail.com>
2023-10-02 18:39:29 -0500

    finished chapter 10 exercise

--- src/main/java/org/launchcode/hellospring/HelloController.java
+++ src/main/java/org/launchcode/hellospring/HelloController.java
@@ -22,14 +22,38 @@ public class HelloController {
     //handles request of the form /hello?name=[name]
 //    @GetMapping("hello") // replaced with RequestMapping to allow both GET and POST
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, @RequestParam String language) {
+        if (name == null) {
+            name = "World";
+        }
+        return helloAgain(name, language);
     }
-
     //handles requests of the form /hello/[name]
     @GetMapping("{name}")
-    public String helloAgain(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloAgain(@PathVariable String name, @PathVariable String language) {
+        String greeting = "";
+
+        if (language.equals("english")) {
+            greeting = "Hello,";
+        }
+        else if (language.equals("spanish")) {
+            greeting = "Hola,";
+        }
+        else if (language.equals("french")) {
+            greeting = "Bonjour,";
+        }
+        else if (language.equals("swedish")) {
+            greeting = "Hallå,";
+        }
+        else if (language.equals("german")) {
+            greeting = "Hallo,";
+        }
+
+        return "<html>" +
+                "<body>" +
+                "<h1>" + greeting + " " + name + "</h1>" +
+                "</body>" +
+                "</html>";
     }
 
     //now lives at /hello/form
@@ -41,6 +65,12 @@ public class HelloController {
                         "<body>" +
                         "<form action = '/hello' method = 'post'>" + //submit a request to /hello because we already have a handler method for this
                         "<input type = 'text' name = 'name' />" +
+                        "<select name='language'>" +
+                            "<option selected = 'selected' value='english'>English</option>" +
+                            "<option value='spanish'>Spanish</option>" +
+                            "<option value='french'>French</option>" +
+                            "<option value='swedish'>Swedish</option>" +
+                            "<option value='german'>German</option>" +
                         "<input type = 'submit' value = 'Greet Me!' />" +
                         "</form>" +
                         "</body>" +
