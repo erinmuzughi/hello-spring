a7601601448e500fedcbef5982c3aceecf839a6d
erinmuzughi <erinmuzughi@gmail.com>
2023-10-02 18:16:39 -0500

    finished next exercise

--- src/main/java/org/launchcode/hellospring/HelloController.java
+++ src/main/java/org/launchcode/hellospring/HelloController.java
@@ -1,9 +1,7 @@
 package org.launchcode.hellospring;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -21,9 +19,32 @@ public class HelloController {
     }
 
     //handles request of the form /hello?name=[name]
-    @GetMapping("hello")
+//    @GetMapping("hello") // replaced with RequestMapping to allow both GET and POST
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
+
+    //handles requests of the form /hello/[name]
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloAgain(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping ("form")
+    @ResponseBody
+    public String helloForm() {
+        return
+                "<html>" +
+                        "<body>" +
+                        "<form action = 'hello' method = 'post'>" + //submit a request to /hello because we already have a handler method for this
+                        "<input type = 'text' name = 'name' />" +
+                        "<input type = 'submit' value = 'Greet Me!' />" +
+                        "</form>" +
+                        "</body>" +
+                        "</html>";
+    }
+
 }
