c1a7576bbd00c91e497d30619485ddc490319dbc
erinmuzughi <erinmuzughi@gmail.com>
2023-10-02 18:22:57 -0500

    finished all chapter exercises

--- src/main/java/org/launchcode/hellospring/HelloController.java
+++ src/main/java/org/launchcode/hellospring/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
 //    @GetMapping("hello")
@@ -11,35 +13,33 @@ public class HelloController {
 //    public String hello() {
 //        return "Hello, Spring!";
 //    }
-
+    //this now lives at /hello/goodbye because we added @RequestMapping above the class
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
     //handles request of the form /hello?name=[name]
 //    @GetMapping("hello") // replaced with RequestMapping to allow both GET and POST
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
     //handles requests of the form /hello/[name]
-    @GetMapping("hello/{name}")
-    @ResponseBody
+    @GetMapping("{name}")
     public String helloAgain(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
+    //now lives at /hello/form
+
     @GetMapping ("form")
-    @ResponseBody
     public String helloForm() {
         return
                 "<html>" +
                         "<body>" +
-                        "<form action = 'hello' method = 'post'>" + //submit a request to /hello because we already have a handler method for this
+                        "<form action = '/hello' method = 'post'>" + //submit a request to /hello because we already have a handler method for this
                         "<input type = 'text' name = 'name' />" +
                         "<input type = 'submit' value = 'Greet Me!' />" +
                         "</form>" +
