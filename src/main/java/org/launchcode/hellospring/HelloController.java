6e8440f2d56e071938e3c6e3803eaad6ba39bad9
erinmuzughi <erinmuzughi@gmail.com>
2023-10-03 09:01:10 -0500

    finished chapter 13 exercises

--- src/main/java/org/launchcode/hellospring/HelloController.java
+++ src/main/java/org/launchcode/hellospring/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloController {
 
@@ -27,6 +30,16 @@ public class HelloController {
     @GetMapping ("form")
     public String helloForm() {
         return "form";
+    }
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("Lauren");
+        names.add("Ashley");
+        names.add("Bri");
+
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 
 }
